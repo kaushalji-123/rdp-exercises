3ade761fe1342718926fcfcab577e2b91c8f9534
kaushalji-123 <divyanshu838283fb@gmail.com>
2023-08-30 09:23:45 +0530

    added talwind css and week2 practice of javaScriprt

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["week_1/css_advanced_and_best_practices/demo-tailw.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
