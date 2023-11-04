b5e598e9f2737c2b5676cc960ad3217d1bc16913
kaushalji-123 <divyanshu838283fb@gmail.com>
2023-11-04 20:09:37 +0530

    Updated week-4 question 1

--- week_3/question_1/Document_1.js
+++ week_3/question_1/Document_1.js
@@ -1,31 +1,31 @@
-const employees = [
-  {
-    name: 'John Doe',
-    age: 30,
-    city: 'New York',
-    occupation: 'Software Engineer'
-  },
-  {
-    name: 'Jane Smith',
-    age: 25,
-    city: 'San Francisco',
-    occupation: 'Web Developer'
-  },
-  {
-    name: 'Bob Johnson',
-    age: 40,
-    city: 'Los Angeles',
-    occupation: 'Data Scientist'
-  }
-];
+// const employees = [
+//   {
+//     name: 'John Doe',
+//     age: 30,
+//     city: 'New York',
+//     occupation: 'Software Engineer'
+//   },
+//   {
+//     name: 'Jane Smith',
+//     age: 25,
+//     city: 'San Francisco',
+//     occupation: 'Web Developer'
+//   },
+//   {
+//     name: 'Bob Johnson',
+//     age: 40,
+//     city: 'Los Angeles',
+//     occupation: 'Data Scientist'
+//   }
+// ];
 
-const updatedEmployees = employees.map((employee) => {
-  employee.city = "New York";
-  return employee;
-});
+// const updatedEmployees = employees.map((employee) => {
+//   employee.city = "New York";
+//   return employee;
+// });
 
-console.log(employees);
-console.log()
+// console.log(employees);
+// console.log()
 
 //            Explanation 
 
@@ -37,3 +37,52 @@ console.log()
   code here we created a new array "updatedEmployees " and
    with the help of "map" function we updated the city object of 
    the original array without modifying the original code.*/
+function deepCopy(obj) {
+  if (typeof obj !== 'object' || obj === null) {
+    return obj;
+  }
+
+  if (Array.isArray(obj)) {
+    return obj.map(deepCopy);
+  }
+
+  const copy = {};
+  for (const key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      copy[key] = deepCopy(obj[key]);
+    }
+  }
+
+  return copy;
+}
+
+let DoCode = {
+  laptop: "open",
+  battery: 100,
+  mind: "learning",
+  distract: {
+    first: "mobile",
+    second: "gf",
+  },
+  function(coding) {
+    return {
+      name: "Divyanshu Kaushal",
+      message: "Let's Start!",
+    };
+  },
+};
+
+let NoCode = deepCopy(DoCode);
+
+NoCode.mind = "sleepy";
+NoCode.distract.first = "Reel";
+
+NoCode.function = function(code) {
+  return {
+    name: "Your Name",
+    message: "let's do",
+  };
+};
+
+console.log("DoCode:", DoCode);
+console.log("NoCode:", NoCode);
---
+++ week_4/Question_1/my-app/src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import SignUp from "./Components/Signup";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World App</h1>
+      <SignUp />
+    </div>
+  );
+}
+
+export default App;
